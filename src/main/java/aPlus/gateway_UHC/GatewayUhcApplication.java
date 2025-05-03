4cd9dc0a53f22f0377a5e8dfb86dbc8430956300
thanhnguyen2406 <nguyencongthanh2406@gmail.com>
2025-05-03 13:50:11 +0700

    fix header for response

--- src/main/java/aPlus/gateway_UHC/GatewayUhcApplication.java
+++ src/main/java/aPlus/gateway_UHC/GatewayUhcApplication.java
@@ -1,6 +1,5 @@
 package aPlus.gateway_UHC;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -13,11 +12,6 @@ import java.io.File;
 public class GatewayUhcApplication {
 
 	public static void main(String[] args) {
-		File envFile = new File(".env");
-		if (envFile.exists() && envFile.isFile()) {
-			Dotenv dotenv = Dotenv.configure().load();
-			System.setProperty("JWT_SIGNER_KEY", dotenv.get("JWT_SIGNER_KEY", ""));
-		}
 		SpringApplication.run(GatewayUhcApplication.class, args);
 	}
 
@@ -28,14 +22,14 @@ public class GatewayUhcApplication {
 						r -> r.path("/user/**")
 								.filters(f -> f
 										.rewritePath("/user/(?<segment>.*)", "/${segment}")
-										.addResponseHeader("X-Custom-Header", "DanSON Gateway Service"))
-								.uri("http://localhost:8081"))
+										)
+								.uri("https://user-domain-1-0-0.onrender.com"))
 
 				.route("appointment_service",
 						r -> r.path("/appointment/**")
 								.filters(f -> f
 										.rewritePath("/user/(?<segment>.*)", "/${segment}")
-										.addResponseHeader("X-Custom-Header", "DanSON Gateway Service"))
+										)
 						.uri("https://appointment-service-e6za.onrender.com"))
 				.build();
 	}
